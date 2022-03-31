55733808d135bed50cab210bb93731ce21f02eb4
Canoes4296 <39389304+Canoes4296@users.noreply.github.com>
2022-03-30 23:15:24 -0400

    'member_descriptor' object has no attribute 'get_role'

--- Core.py
+++ Core.py
@@ -2,7 +2,7 @@ import discord
 import pickle
 # from erplbot.club_members import get_members_from_spreadsheet, Name
 from Oauth import discord_token, retrieve_credentials
-from MemberHandler import OFFICER_ROLE_ID, PROJECT_ROLE_ID, MEMBER_ROLE_ID, RECRUIT_ROLE_ID, BOT_COMMAND_CHANNEL, \
+from MemberHandler import JOIN_CHANNEL, OFFICER_ROLE_ID, PROJECT_ROLE_ID, MEMBER_ROLE_ID, RECRUIT_ROLE_ID, BOT_COMMAND_CHANNEL, JOIN_CHANNEL, \
     guild_ID
 from MemberHandler import Member_Handler
 from EventsHandler import GoogleCalendar
@@ -26,7 +26,50 @@ class ERPLBot(discord.Client):
         # GoogleCalendar.initEvents() # Not yet working
         # Member_Handler.initMember() # Comment in to work with: on_member_join(self, member)
       
-      
+    async def on_member_join(self, member):
+        """
+        This function runs whenever a new member joins the server
+        """
+        # Ignore our own updates
+        if member == self.user:
+            return
+
+        print(f"{member.name} joined")
+        # Give em' the default role
+        recruit_role = discord.Member.guild.get_role(RECRUIT_ROLE_ID)
+        # recruit_role = RECRUIT_ROLE_ID # Remove after finished
+        await member.add_roles(recruit_role, reason ='Member join', atomic = True)
+        # Create the DM by default
+        await member.create_dm()
+        async with member.typing():
+            # Check as soon as they've joined
+            await self.update_members(member)
+            # Add a welcome message/embed here
+            embed = discord.Embed(
+                title="*We hope you rocket to success with us!* :rocket: <:ERPL:809226558988484608>",
+                colour=discord.Colour(0x255c6),
+                description=f"<@{member.id}> Welcome to **ERPL**! Please read our rules on <#{751973296114761788}>.\r\n If you've paid dues, Please set your nick to the name you filled out in payment of dues...\n *<@{801184786580242552}> should do the rest. This will get you access to project channels.*")
+            embed.set_thumbnail(url="https://discord.com/assets/748ff0e7b2f1f22adecad8463de25945.svg")
+            embed.set_author(name="Welcome to the Experimental Rocket Propulsion Lab!")
+            await member.guild.get_channel(JOIN_CHANNEL).send(embed=embed)
+    
+    async def on_member_leave(self, discord_member):
+        """
+        This function runs whenever a new member leaves the server
+        """
+    
+    
+    async def on_member_update(self, before, after):
+        """
+        This function runs whenever a new member updates their own profile, like changing their nickname
+        """
+        if before.display_name != after.display_name:
+            print(f"{before.name} updated to {after.display_name}")
+            # Ignore our own updates
+            if after == self.user:
+                return
+            # Here we will just call the update_members function
+            await self.update_members(after.member)
 
     async def on_create_project(self, message):
         """
--- MemberHandler.py
+++ MemberHandler.py
@@ -2,8 +2,7 @@ import discord
 import pickle
 # from erplbot.club_members import get_members_from_spreadsheet, Name
 from discord import client, guild
-from discord.ext.commands import bot
-from discord.ext import tasks
+from discord.ext import commands, tasks; from discord.ext.commands import bot
 
 from Oauth import  retrieve_credentials, Google_SPREADSHEET_ID, RANGE
 #from __future__ import print_function
@@ -78,75 +77,80 @@ class Name:
 
 class Member_Handler(discord.Client):
 
-    def initMember():
-        Member_Handler.initializeMemberHandler.start()
+    # def initMember():
+    #     Member_Handler.initializeMemberHandler.start()
 
-    def unload(self):
-        self.initializeMemberHandler.cancel()
+    # def unload(self):
+    #     self.initializeMemberHandler.cancel()
 
-    @tasks.loop(seconds = 20)
-    async def initializeMemberHandler():
-        # await Member_Handler().on_member_join(None)
-        # loop = asyncio.new_event_loop()
-        # tasks = Member_Handler.on_member_join(Member_Handler, Member_Handler.user)
-        # (loop.run_until_complete(tasks))
-        # asyncio.to_thread
-        await Member_Handler.on_member_join(Member_Handler, discord.Member)
+    # @tasks.loop(seconds = 20)
+    # async def initializeMemberHandler():
+    #     # await Member_Handler().on_member_join(None)
+    #     # loop = asyncio.new_event_loop()
+    #     # tasks = Member_Handler.on_member_join(Member_Handler, Member_Handler.user)
+    #     # (loop.run_until_complete(tasks))
+    #     # asyncio.to_thread
+    #     await Member_Handler.on_member_join(Member_Handler, discord.Member)
 
-    async def on_member_join(self, member):
-        """
-        This function runs whenever a new member joins the server
-        """
-        # Ignore our own updates
-        if member == self.user:
-            return
-        # print(discord.Client.fetch_guilds(guild_ID).get_role(RECRUIT_ROLE_ID).name) 
-        print(f"{member.name} joined")
-        # Give em' the default role
-        recruit_role = member.guild.get_role(RECRUIT_ROLE_ID)
-        # recruit_role = RECRUIT_ROLE_ID # Remove after finished
-        await member.add_roles(recruit_role, reason ='Member join')
-        # Create the DM by default
-        await member.create_dm()
-        async with member.typing():
-            # Check as soon as they've joined
-            await self.update_members(member)
-            # Add a welcome message/embed here
-            embed = discord.Embed(
-                title="*We hope you rocket to success with us!* :rocket: <:ERPL:809226558988484608>",
-                colour=discord.Colour(0x255c6),
-                description=f"<@{member.id}> Welcome to **ERPL**! Please read our rules on <#{751973296114761788}>.\r\n If you've paid dues, Please set your nick to the name you filled out in payment of dues...\n *<@{801184786580242552}> should do the rest. This will get you access to project channels.*")
-            embed.set_thumbnail(url="https://discord.com/assets/748ff0e7b2f1f22adecad8463de25945.svg")
-            embed.set_author(name="Welcome to the Experimental Rocket Propulsion Lab!")
-            await member.guild.get_channel(JOIN_CHANNEL).send(embed=embed)
+    # async def on_member_join(self, member):
+    #     """
+    #     This function runs whenever a new member joins the server
+    #     """
+    #     # Ignore our own updates
+    #     if member == self.user:
+    #         return
+
+    #     print(f"{member.name} joined")
+    #     # Give em' the default role
+    #     recruit_role = discord.Member.guild.get_role(RECRUIT_ROLE_ID)
+    #     # recruit_role = RECRUIT_ROLE_ID # Remove after finished
+    #     await member.add_roles(recruit_role, reason ='Member join')
+    #     # Create the DM by default
+    #     await member.create_dm()
+    #     async with member.typing():
+    #         # Check as soon as they've joined
+    #         await self.update_members(member)
+    #         # Add a welcome message/embed here
+    #         embed = discord.Embed(
+    #             title="*We hope you rocket to success with us!* :rocket: <:ERPL:809226558988484608>",
+    #             colour=discord.Colour(0x255c6),
+    #             description=f"<@{member.id}> Welcome to **ERPL**! Please read our rules on <#{751973296114761788}>.\r\n If you've paid dues, Please set your nick to the name you filled out in payment of dues...\n *<@{801184786580242552}> should do the rest. This will get you access to project channels.*")
+    #         embed.set_thumbnail(url="https://discord.com/assets/748ff0e7b2f1f22adecad8463de25945.svg")
+    #         embed.set_author(name="Welcome to the Experimental Rocket Propulsion Lab!")
+    #         await member.guild.get_channel(JOIN_CHANNEL).send(embed=embed)
             
-    async def on_member_leave(self, discord_member):
-        """
-        This function runs whenever a new member leaves the server
-        """
+    # async def on_member_leave(self, discord_member):
+    #     """
+    #     This function runs whenever a new member leaves the server
+    #     """
     
     
-    async def on_member_update(self, before, after):
-        """
-        This function runs whenever a new member updates their own profile, like changing their nickname
-        """
-        if before.display_name != after.display_name:
-            print(f"{before.name} updated to {after.display_name}")
-            # Ignore our own updates
-            if after == self.user:
-                return
-            # Here we will just call the update_members function
-            await self.update_members(after.member)
+    # async def on_member_update(self, before, after):
+    #     """
+    #     This function runs whenever a new member updates their own profile, like changing their nickname
+    #     """
+    #     if before.display_name != after.display_name:
+    #         print(f"{before.name} updated to {after.display_name}")
+    #         # Ignore our own updates
+    #         if after == self.user:
+    #             return
+    #         # Here we will just call the update_members function
+    #         await self.update_members(after.member)
             
 
-    async def on_message(self, message):
-        """
-        This function runs whenever a message is sent
-        """
+    # async def on_message(self, message):
+    #     """
+    #     This function runs whenever a message is sent
+    #     """
 
-        # Ignore our own updates
-        if message.author == self.user:
-            return
+    #     # Ignore our own updates
+    #     if message.author == self.user:
+    #         return
+        
+    #     # Recognize if the message is a DM
+    #     if message.content.startswith('/DM'):
+    #         msg = 'This Message is send in DM'
+    #         await client.send_message(message.author,)
         
         
     async def update_member(self, member):
